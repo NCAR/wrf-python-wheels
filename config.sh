29d8f859fc3a4a7d0d8d1f0b4029a8b7cfbabc3a
Maik Riechert <maik.riechert@arcor.de>
2017-12-27 23:32:22 +0100

    use mingw on appveyor (for gfortran)

--- config.sh
+++ config.sh
@@ -10,5 +10,5 @@ function pre_build {
 function run_tests {
     # Runs tests on installed distribution from an empty directory
     python --version
-    python -c "import wrf-python"
+    python -c "import wrf"
 }
