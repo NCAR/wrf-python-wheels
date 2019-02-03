fd7187bd1faa3958f158b9e3ac676b41c0ef290c
Maik Riechert <maik.riechert@arcor.de>
2019-02-03 23:10:25 +0000

    update to latest OpenMP-enabled wrf-python, remove Python < 3.5 support

--- config.sh
+++ config.sh
@@ -3,13 +3,24 @@
 # See env_vars.sh for extra environment variables
 source gfortran-install/gfortran_utils.sh
 
+function pre_build {
+    pushd $REPO_DIR/fortran/build_help
+    gfortran -o sizes -fopenmp omp_sizes.f90
+    python sub_sizes.py
+    popd
+
+    pushd $REPO_DIR/fortran
+    gfortran -E ompgen.F90 -fopenmp -cpp -o omp.f90
+    popd
+}
+
 function build_wheel {
+    export FFLAGS="-fPIC -fopenmp -mtune=generic"
+    export LDFLAGS="-fPIC -fopenmp"
     if [ -z "$IS_OSX" ]; then
-        unset FFLAGS
-        export LDFLAGS="-shared -Wl,-strip-all"
+        export LDFLAGS="$LDFLAGS -shared -Wl,-strip-all"
         build_pip_wheel $@
     else
-        export FFLAGS="$FFLAGS -fPIC"
         build_osx_wheel $@
     fi
 }
@@ -18,23 +29,24 @@ function set_arch {
     local arch=$1
     export CC="clang $arch"
     export CXX="clang++ $arch"
-    export CFLAGS="$arch"
-    export FFLAGS="$arch"
+    export CFLAGS="$CFLAGS $arch"
+    export FFLAGS="$FFLAGS $arch"
     export FARCH="$arch"
-    export LDFLAGS="$arch"
+    export LDFLAGS="$LDFLAGS $arch"
 }
 
 function build_osx_wheel {
     # Build 64-bit wheel
     # Standard gfortran won't build dual arch objects.
     local repo_dir=${1:-$REPO_DIR}
-    local py_ld_flags="-Wall -undefined dynamic_lookup -bundle"
+    local py_ld_flags="-Wall -undefined dynamic_lookup -bundle -fopenmp"
 
     install_gfortran
     # 64-bit wheel
     local arch="-m64"
     set_arch $arch
     # Build wheel
+    #export FFLAGS="$FFLAGS -fPIC"
     export LDSHARED="$CC $py_ld_flags"
     export LDFLAGS="$arch $py_ld_flags"
     build_pip_wheel "$repo_dir"
@@ -44,4 +56,9 @@ function run_tests {
     # Runs tests on installed distribution from an empty directory
     python --version
     python -c "import wrf"
+    # $REPO_DIR not available here, see multibuild/travis_linux_steps.sh (install_run)
+    local repo_dir="../wrf-python"
+    pushd $repo_dir/test/ci_tests
+    python utests.py
+    popd
 }
